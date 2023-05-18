6137c906e2c51ac113437b8ee4ea3f6a9c8b64b7
jhony2488 <jhon.araujo2488@gmail.com>
2023-05-18 14:43:47 -0300

    configurado style guide sequelizer docker e tsconfig

---
+++ server/doc.ts
@@ -0,0 +1,44 @@
+import version from 'project-version'
+
+const doc = {
+  info: {
+    version,
+    title: 'api-prospects',
+    description: '',
+  },
+  host: 'localhost:80',
+  basePath: '/',
+  schemes: ['http', 'https'],
+  consumes: ['application/json'],
+  produces: ['application/json'],
+  tags: [
+    {
+      name: 'Default',
+      description: 'Endpoints of default endpoints',
+    },
+  ],
+  securityDefinitions: {
+    api_key: {
+      type: 'apiKey',
+      name: 'auth-token',
+      in: 'header',
+    },
+  },
+  definitions: {
+    Error400: {
+      message: 'Descrição sobre o erro',
+    },
+    Error406: {
+      message: 'Descrição sobre o erro',
+    },
+    Error403: {
+      message: 'Descrição sobre o erro',
+    },
+    ErrorTokenInvalid: { message: 'Token Invalid || Token not provided' },
+    DefaultIndex: {
+      version,
+    },
+  },
+}
+
+module.exports = doc
