c5a25975439a59c80a6fd016d5e450eb9c66b8d9
jhony2488 <jhon.araujo2488@gmail.com>
2023-05-20 15:18:40 -0300

    configurado arquivo .env.example

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders button search react with pesquisar in content button', () => {
   render(<App />);
-  const linkElement = screen.getByText(/pesquisar/i);
+  const linkElement = screen.getByText(/tarefas/i);
   expect(linkElement).toBeInTheDocument();
 });
